4c9f2c03ddf2870d9a4e054f7bc3c6d6c3540bc4
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 22:27:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {|e| e == cheese_types.each_char { |chr| chr}}
-  
+
 end
