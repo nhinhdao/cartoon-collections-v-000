55d5db79fcb8067003c7c1703287aa1ce62984b3
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 22:13:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map { |e|  e[0].upcase}
+  array.map { |e|  e[0].upcase + !}
 end
 
 def long_planeteer_calls# code an argument here
