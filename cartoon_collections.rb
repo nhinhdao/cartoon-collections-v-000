560c159bf0164dec460cda8929e802a7cdda0bf0
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 22:21:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.all? { |e| e.length > 4 }
+  array.any? { |e| e.length > 4 }
 end
 
 def find_the_cheese# code an argument here
