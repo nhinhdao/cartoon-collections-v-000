24aa0007b24f00e97f33f81c4e8f943050b15cd5
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 22:30:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map { |e|  e + '!'}
-  array.map { |e|  e[0].upcase}
+  array.map { |e|  e.first.upcase}
 end
 
 def long_planeteer_calls(array)# code an argument here
@@ -18,5 +18,4 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {|e| cheese_types.include?(e)}
-
 end
